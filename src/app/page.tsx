e6af1fa9240b40c27e8e32b5580078b852128acc
hamzaErraji-debug <errajihamza80@gmail.com>
2026-02-10 15:15:35 +0100

    install tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="text-red-600">
+      hello
     </div>
   );
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss"
+import animate from "tailwindcss-animate"
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [animate],
+}
+
+export default config
